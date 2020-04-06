5ad3b62d5c158592d559792a11dc50ce4f6c2c91
akiakiS <35886966+akiakishitai@users.noreply.github.com>
2020-04-06 23:09:53 +0900

    chore: install plugins of sass and markdown
    - chore: install sass
    - refactor: use scss in style of vue
    - chore: install markdownit
    - feat: create a plugin for markdown-it
    - feat: render demo page of markdown
    
    close #28
    close #29

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '@/plugins/markdown-it.ts' }],
   /*
    ** Nuxt.js dev-modules
    */
@@ -57,7 +57,13 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {},
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.md$/,
+        loader: 'raw-loader',
+        exclude: /(node_modules)/,
+      })
+    },
   },
   /*
    ** Router
