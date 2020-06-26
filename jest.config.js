0646d1ba5a74b95585005d2ba5f52f310b69998b
akiakiS <35886966+akiakishitai@users.noreply.github.com>
2020-06-26 15:03:53 +0900

    fix: fix to show/hide elements on each page
    - feat: create mixin for checking debug on article
    
    - fix: fix to check debug
    
    close #65, close #66

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,6 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/components/**/*.vue',
     //'<rootDir>/src/pages/**/*.vue',
+    '<rootDir>/src/mixins/**/*.ts',
   ],
 }
--- src/components/templates/TopPage.spec.ts
+++ src/components/templates/TopPage.spec.ts
@@ -2,23 +2,30 @@ import { shallowMount, Wrapper } from '@vue/test-utils'
 import TopPage from './TopPage.vue'
 
 describe('TopPage', () => {
-  test('is vue', () => {
-    const wrapper = shallowMount(TopPage, {
-      propsData: {
-        contents: {
+  const createProps = (content?: object) => {
+    return {
+      contents: [
+        {
           path: 'hoge/foo/bar.md',
           filename: 'bar.md',
           filename_noext: 'bar',
           title: 'Example',
           created_at: '2020-06-22',
           tags: ['hoge', 'foo'],
+          ...content,
         },
-        paging: {
-          current: 2,
-          pages: [1, 2, 3, 4, 5, 6],
-        },
-        route: '/posts',
+      ],
+      paging: {
+        current: 2,
+        pages: [1, 2, 3, 4, 5, 6],
       },
+      route: '/posts',
+    }
+  }
+
+  test('is vue', () => {
+    const wrapper = shallowMount(TopPage, {
+      propsData: createProps(),
       stubs: {
         HeadingLevel: true,
         Pagination: true,
@@ -28,5 +35,15 @@ describe('TopPage', () => {
     //console.log(wrapper.html())
 
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('does notDebugContents', () => {
+    let instance = new TopPage({ propsData: createProps() }) as any
+    expect(instance.notDebugContents.length).toBe(1)
+
+    instance = new TopPage({
+      propsData: createProps({ tags: ['draft', 'hoge', 'foo'] }),
+    }) as any
+    expect(instance.notDebugContents.length).toBe(0)
   })
 })
