64517ad9472cb688c5bd2b477aecbbf47924bcfe
akiakiS <35886966+akiakishitai@users.noreply.github.com>
2020-08-26 16:55:56 +0900

    refactor: separate stylesheet to other file in article
    - refactor: change to 'require' from 'import'
    - refactor: separate to stylesheet files
    - chore: set a stylesheet for vscode preview
    
    fix #111

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -7,8 +7,10 @@
 
 /* eslint-env node, es6 */
 
-import { opacity } from 'tailwindcss/defaultTheme'
-import palette from 'material-colors'
+/** @type {{opacity: Record<string, string>}} */
+const { opacity } = require('tailwindcss/defaultTheme')
+/** @type {import('material-colors/dist/colors.json')} */
+const palette = require('material-colors/dist/colors.json')
 
 function customColorProperty(type, colorName) {
   const propertyMap = new Map([
