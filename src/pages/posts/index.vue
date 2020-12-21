d1c4dcc3a8a3722216d2731d18a033b18a78a90b
akiakiS <35886966+akiakishitai@users.noreply.github.com>
2020-12-21 16:17:42 +0900

    fix: redirect to home from '/posts' route

---
+++ src/pages/posts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>redirect to home.</div>
+</template>
+
+<script lang="ts">
+import { Context } from '@nuxt/types'
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class index extends Vue {
+  async asyncData(ctx: Context) {
+    return ctx.redirect('/')
+  }
+}
+</script>
